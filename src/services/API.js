7d2bc2a1379dfc54c3ceceab6622237e6677d7f8
Pera <peratrusnik@gmail.com>
2023-02-06 13:35:32 +0100

    update API

--- src/services/API.js
+++ src/services/API.js
@@ -1,10 +1,10 @@
 class API{
     static getAllTags = async () => {
-        return await fetch("https://raw.githubusercontent.com/zile028/fake-db/main/tags")
+        return await fetch("https://raw.githubusercontent.com/peratrusnik/fake_db/main/tags")
         .then((res)=> res.json())
     }
     static getAllPost = () => {
-        return fetch("https://raw.githubusercontent.com/zile028/fake-db/main/posts")
+        return fetch("https://raw.githubusercontent.com/peratrusnik/fake_db/main/posts")
         .then((res) => res.json())
     }
 }
